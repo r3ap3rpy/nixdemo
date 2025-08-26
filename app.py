76863ac122c1946aea8285c73807723bf99dca2c
Daniel <r3ap3rpy@gmail.com>
2025-08-26 17:05:00 +0200

    Revert "My summary"
    This reverts commit 92446f7918894e53bc919ee76326e71a8151d9fc.
    
    So cool

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import this
-a
-b
-c
-d
-e
-f
 def equals(a,b):
     return a == b
-print("amend")
+
 print("THis is cool")
 print("Local branch")
 print("sadfakdjakdj")
 print("A new feature is incoming!")
-print("Stash demo!")
